5cd2465b93f2ac832dd8b33ecaa108f7338e3035
cmtran09 <cmtran@hotmail.co.uk>
2020-04-28 16:17:09 +0100

    table is working

--- src/components/Last5GamesTable/Last5GamesTable.js
+++ src/components/Last5GamesTable/Last5GamesTable.js
@@ -19,48 +19,46 @@ export default function Last5GamesTable(props) {
 
   const classes = useStyles();
 
+  const [table, setTable] = useState([])
+
   function createData(home, homeScore, visitingScore, visiting) {
     return { home, homeScore, visitingScore, visiting };
   }
 
-  let rows = []
+
+
+  // console.log("outside useeffect", rows)
 
   useEffect(() => {
+    let rows = []
+    console.log("porps [props.data[0].home_team.name", [props.data[0].home_team.name])
+
     for (let i = 5; i > 0; i--) {
       rows.push(createData(props.data[i].home_team.name, props.data[i].home_team_score, props.data[i].visitor_team_score, props.data[i].visitor_team.name))
     }
+
+    console.log("porps [props.data[0].home_team.nameROWWWWWSA", rows)
+    setTable(rows)
   }, [props.data[0].home_team.name])
-
-
-  // if (props.data) {
-  //   var rows =
-  //     [
-  //       createData(props.data[5].home_team.name, props.data[5].home_team_score, props.data[5].visitor_team_score, props.data[5].visitor_team.name)
-  //       // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  //       // createData('Eclair', 262, 16.0, 24, 6.0),
-  //       // createData('Cupcake', 305, 3.7, 67, 4.3),
-  //       // createData('Gingerbread', 356, 16.0, 49, 3.9),
-  //     ]
-  // }
 
 
   // console.log("table", props.data[0].home_team.name)
   return (
     <div>
       hello table
-      {/* <TableContainer component={Paper}>
+      <TableContainer component={Paper}>
         <Table className={classes.table} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
               <TableCell align="center">Home Team</TableCell>
-              <TableCell align="center">Home Team Score</TableCell>
-              <TableCell align="center">Visiting Team Score</TableCell>
+              <TableCell align="center">Score</TableCell>
+              <TableCell align="center">Score</TableCell>
               <TableCell align="center">Visiting Team</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.home}>
+            {table.map((row, i) => (
+              <TableRow key={i}>
                 <TableCell align="center" component="th" scope="row">
                   {row.home}
                 </TableCell>
@@ -71,8 +69,10 @@ export default function Last5GamesTable(props) {
             ))}
           </TableBody>
         </Table>
-      </TableContainer> */}
-      <button onClick={e => console.log(props.data[0].home_team.name)}>buttoin</button>
+      </TableContainer>
+
+      <button onClick={e => console.log(table)}>buttoin</button>
+      {/* <button onClick={e => console.log(willTable)}>buttoin</button> */}
     </div>
   )
 }
